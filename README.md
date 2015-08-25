8d439de37dce04492d90a2366f4de828c5777567
petrnejedly <pnejedly@equalexperts.com>
2015-08-25 15:09:55 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,20 @@
+play-url-binders
+================
+
+[![Build Status](https://travis-ci.org/hmrc/play-url-binders.svg)](https://travis-ci.org/hmrc/play-url-binders) [ ![Download](https://api.bintray.com/packages/hmrc/releases/play-url-binders/images/download.svg) ](https://bintray.com/hmrc/releases/play-url-binders/_latestVersion)
+
+This library includes some basic functionality related to binding URL parameters and query string values in Play controller routes.
+
+### Installing
+
+Add the following to your SBT build:
+```scala
+resolvers += Resolver.bintrayRepo("hmrc", "releases")
+
+libraryDependencies += "uk.gov.hmrc" % "play-url-binders" % "[INSERT-VERSION]"
+```
+
+## License ##
+
+This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
+
---
+++ project/HmrcBuild.scala
@@ -0,0 +1,45 @@
+import sbt.Keys._
+import sbt._
+
+object HmrcBuild extends Build {
+
+  import uk.gov.hmrc.DefaultBuildSettings._
+  import uk.gov.hmrc.SbtAutoBuildPlugin
+  import uk.gov.hmrc.versioning.SbtGitVersioning
+
+  val appName = "play-url-binders"
+
+  val appDependencies = Dependencies.compile ++ Dependencies.test
+
+
+  lazy val library = Project(appName, file("."))
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
+    .settings(
+      targetJvm := "jvm-1.7",
+      libraryDependencies ++= appDependencies,
+      crossScalaVersions := Seq("2.11.7"),
+      resolvers := Seq(
+        Resolver.bintrayRepo("hmrc", "releases"),
+        Resolver.typesafeRepo("releases")
+      )
+    )
+    .disablePlugins(sbt.plugins.JUnitXmlReportPlugin)
+
+}
+
+object Dependencies {
+
+  import play.core.PlayVersion
+
+  val compile = Seq(
+    "com.typesafe.play" %% "play" % PlayVersion.current
+  )
+
+  val test = Seq(
+    "org.scalatest" %% "scalatest" % "2.2.2" % "test",
+    "org.pegdown" % "pegdown" % "1.4.2" % "test"
+  )
+
+}
+
+
